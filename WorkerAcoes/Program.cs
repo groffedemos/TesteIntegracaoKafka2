316dbe1c20c7ac6cc29a9bc62802a62681851bab
Renato Groffe <renato.groffe@yahoo.com.br>
2021-04-26 14:21:49 -0300

    Commit inicial
    Commit inicial

---
+++ WorkerAcoes/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkerAcoes.Data;
+
+namespace WorkerAcoes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<AcoesRepository>();
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
